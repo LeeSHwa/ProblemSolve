344f3d19b998815d1d8903767a547805072fb07f
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-13 15:30:51 +0900

    Title: 상한 귤, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/상한 귤/oranges-have-gone-bad.py
+++ 260413/상한 귤/oranges-have-gone-bad.py
@@ -3,23 +3,17 @@ from collections import deque
 def solve():
     n, k = map(int, input().split())
 
-    grid = []
     rotten_mandarin = [[0] * n for _ in range(n)]
     starts = deque()
 
-    visited = [[False] * n for _ in range(n)]
-
     for row in range(n):
         line = list(map(int, input().split()))
-
-        grid.append(line)
 
         for col in range(n):
             if line[col] == 0:
                 rotten_mandarin[row][col] = -1
             elif line[col] == 2:
                 starts.append((row, col, 0))
-                visited[row][col] = True
             else:
                 rotten_mandarin[row][col] = -2
 
@@ -30,10 +24,9 @@ def solve():
             nr = row + dr
             nc = col + dc
 
-            if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc] and grid[nr][nc] == 1:
+            if 0 <= nr < n and 0 <= nc < n and rotten_mandarin[nr][nc] == -2:
                 starts.append((nr, nc, cnt + 1))
                 rotten_mandarin[nr][nc] = cnt + 1
-                visited[nr][nc] = True
 
     return rotten_mandarin
 
