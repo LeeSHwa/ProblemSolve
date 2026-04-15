f2a00d151f6cfd72a28dc350187a51d71cf5bc8a
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-15 16:22:14 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/사각형 채우기 3/rectangle-fill-3.py
+++ 260415/사각형 채우기 3/rectangle-fill-3.py
@@ -3,6 +3,7 @@ n = int(input())
 # Please write your code here.
 UNUSED = -1
 MAX_N = 1001
+MOD = 1000000007
 
 memo = [UNUSED for _ in range(MAX_N)]
 memo[0] = 1
@@ -10,9 +11,9 @@ memo[1] = 2
 memo[2] = 7
 
 for i in range(3, n + 1):
-    memo[i] = memo[i-1] * 2 + memo[i-2] * 3
+    memo[i] = (memo[i-1] * 2 + memo[i-2] * 3) % MOD
     
     for j in range(i - 3, -1, -1):
-        memo[i] = memo[i] + memo[j] * 2
+        memo[i] = (memo[i] + memo[j] * 2) % MOD
 
 print(memo[n])
