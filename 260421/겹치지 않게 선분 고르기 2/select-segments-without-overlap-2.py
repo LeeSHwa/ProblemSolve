a1adbcac911afb976638dc88089290e704b43137
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-21 11:21:11 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 260421/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -1,9 +1,13 @@
 n = int(input())
 
-line = {}
 MAX_RANGE = 1001
 
 dp = [0] * MAX_RANGE
+
+# pos = [tuple(map(int, input().split())) for _ in range(n)]
+# pos.sort(key = lambda x : x[1])
+
+line = {}
 
 for _ in range(n):
     x1, x2 = map(int, input().split())
@@ -17,11 +21,11 @@ pos = sorted(line.items(), key = lambda x : x[1])
 
 for x1, x2 in pos:
     
-    max_value = 0
+    max_value = -1
     for end in range(x1):
         max_value = max(max_value, dp[end])
     
-    dp[x2] = max_value + 1
+    dp[x2] = max(dp[x2], max_value + 1)
 
 print(max(dp))
     
