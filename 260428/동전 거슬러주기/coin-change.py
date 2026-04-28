b3f23f79fb4cf1a0d25e9b535e3efedfffb944ca
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-28 15:33:29 +0900

    Title: 동전 거슬러주기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260428/동전 거슬러주기/coin-change.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+coins = list(map(int, input().split()))
+
+dp = [float('inf')] * (m + 1)
+
+for i in coins:
+    dp[i] = 1
+
+for i in range(1, m + 1):
+
+    for coin in coins:
+        if i - coin > 0:
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    
+if dp[-1] == float('inf'):
+    print(-1)
+else:
+    print(dp[-1])
