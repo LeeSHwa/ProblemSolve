8c9538b9820b186db4a08d6ffb4cfd82bfb713fd
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-29 11:08:23 +0900

    Title: 부분 수열의 합이 M, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
+++ 260429/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -8,8 +8,8 @@ dp[0] = 0
 
 for num in nums:
     
-    for idx in range(m, -1, -1):
-        if idx >= num and dp[idx - num] != float('inf'):
+    for idx in range(m, num - 1, -1):
+        if dp[idx - num] != float('inf'):
             dp[idx] = min(dp[idx], dp[idx - num] + 1)
 
 print(dp[-1] if dp[-1] != float('inf') else -1)
