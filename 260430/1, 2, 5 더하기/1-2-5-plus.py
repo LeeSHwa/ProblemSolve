5f1ced6d088654fef06b72926044b9493b262168
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-30 11:28:05 +0900

    Title: 1, 2, 5 더하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+MOD = 10007
+
+dp = [float('inf')] * (n + 6)
+
+# 초기상태 정의
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+dp[4] = 5
+dp[5] = 9
+
+
+for i in range(6, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 5]
+
+print(dp[n])
