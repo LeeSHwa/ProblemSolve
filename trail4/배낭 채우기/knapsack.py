de007994baafbe0ef51931b5c00059c278e6270b
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-05-02 15:13:19 +0900

    [Passed] 배낭 채우기 (100ms, 30MB)

--- trail4/배낭 채우기/knapsack.py
+++ trail4/배낭 채우기/knapsack.py
@@ -6,28 +6,30 @@ def solve():
     UNUSED = -float('inf')
 
     # i번째 보석까지 고려했을 때, j의 무게에서 얻을 수 있는 최대의 가치
-    dp = [[UNUSED] * (m + 1) for _ in range(n)]
+    dp = [[UNUSED] * (m + 1) for _ in range(n + 1)]
     dp[0][0] = 0
 
     #누더기1
-    if jewels[0][0] <= m:
-        dp[0][jewels[0][0]] = jewels[0][1]
+    # if jewels[0][0] <= m:
+    #     dp[0][jewels[0][0]] = jewels[0][1]
 
-    for i in range(1, n):
+    for i in range(1, n + 1):
 
-        wei, price = jewels[i]
+        wei, price = jewels[i - 1]
         
         #누더기2
-        if m >= wei:
-            dp[i][wei] = price
+        # if m >= wei:
+        #     dp[i][wei] = price
 
         for j in range(m + 1):
             #누더기3
-            if j >= wei and dp[i - 1][j - wei] != UNUSED:
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - wei] + price)
+            if j >= wei:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wei] + price)
 
             #누더기4
-            dp[i][j] = max(dp[i][j], dp[i-1][j])
+            else:
+                dp[i][j] = max(dp[i][j], dp[i-1][j])
+
 
     #누더기5
     return max(dp[-1])
